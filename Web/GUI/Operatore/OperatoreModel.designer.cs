1bad168106d109b2f81e46987b88f8a1be24adac
miriam <miriam.iusi@gmail.com>
2014-11-09 14:52:19 +0100

    dashboard

--- Web/GUI/Operatore/OperatoreModel.designer.cs
+++ Web/GUI/Operatore/OperatoreModel.designer.cs
@@ -30,14 +30,44 @@ namespace Web.GUI.Operatore
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.editEmail = new Library.Template.Controls.TemplateEditEmail();
+            this.editCognome = new Library.Template.Controls.TemplateEditText();
+            this.editConfermaPassword = new Library.Template.Controls.TemplateEditPassword();
+            this.editPassword = new Library.Template.Controls.TemplateEditPassword();
+            this.editNome = new Library.Template.Controls.TemplateEditText();
+            this.editNomeUtente = new Library.Template.Controls.TemplateEditText();
+            this.editTelefono = new Library.Template.Controls.TemplateEditText();
+            this.editAmministratore = new Library.Template.Controls.TemplateEditCheckBox();
+            this.editUtente = new Library.Template.Controls.TemplateEditCheckBox();
+            this.editSupervisor = new Library.Template.Controls.TemplateEditCheckBox();
             this.container.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.infoSubtitleImage)).BeginInit();
             this.SuspendLayout();
             // 
             // container
             // 
+            this.container.Controls.Add(this.editSupervisor);
+            this.container.Controls.Add(this.editUtente);
+            this.container.Controls.Add(this.editAmministratore);
+            this.container.Controls.Add(this.editTelefono);
+            this.container.Controls.Add(this.editNomeUtente);
+            this.container.Controls.Add(this.editNome);
+            this.container.Controls.Add(this.editPassword);
+            this.container.Controls.Add(this.editConfermaPassword);
+            this.container.Controls.Add(this.editCognome);
+            this.container.Controls.Add(this.editEmail);
             this.container.Controls.SetChildIndex(this.infoSubtitleImage, 0);
             this.container.Controls.SetChildIndex(this.infoSubtitle, 0);
+            this.container.Controls.SetChildIndex(this.editEmail, 0);
+            this.container.Controls.SetChildIndex(this.editCognome, 0);
+            this.container.Controls.SetChildIndex(this.editConfermaPassword, 0);
+            this.container.Controls.SetChildIndex(this.editPassword, 0);
+            this.container.Controls.SetChildIndex(this.editNome, 0);
+            this.container.Controls.SetChildIndex(this.editNomeUtente, 0);
+            this.container.Controls.SetChildIndex(this.editTelefono, 0);
+            this.container.Controls.SetChildIndex(this.editAmministratore, 0);
+            this.container.Controls.SetChildIndex(this.editUtente, 0);
+            this.container.Controls.SetChildIndex(this.editSupervisor, 0);
             // 
             // infoSubtitle
             // 
@@ -47,11 +77,237 @@ namespace Web.GUI.Operatore
             // 
             this.infoSubtitleImage.Location = new System.Drawing.Point(610, 3);
             // 
-            // infoDescription
+            // editEmail
             // 
+            this.editEmail.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editEmail.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editEmail.BackColor = System.Drawing.Color.Transparent;
+            this.editEmail.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editEmail.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editEmail.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editEmail.Changed = false;
+            this.editEmail.Editing = false;
+            this.editEmail.Label = "Email";
+            this.editEmail.LabelWidth = 175;
+            this.editEmail.Location = new System.Drawing.Point(20, 330);
+            this.editEmail.Name = "templateEditEmail1";
+            this.editEmail.ReadOnly = false;
+            this.editEmail.Required = false;
+            this.editEmail.Size = new System.Drawing.Size(800, 30);
+            this.editEmail.TabIndex = 2;
+            this.editEmail.Text = "TemplateEditEmail";
+            this.editEmail.Value = "";
             // 
-            // editCodice
+            // editCognome
             // 
+            this.editCognome.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editCognome.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editCognome.BackColor = System.Drawing.Color.Transparent;
+            this.editCognome.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editCognome.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editCognome.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editCognome.Changed = false;
+            this.editCognome.Editing = false;
+            this.editCognome.Label = "Cognome";
+            this.editCognome.LabelWidth = 175;
+            this.editCognome.Location = new System.Drawing.Point(20, 75);
+            this.editCognome.Multiline = false;
+            this.editCognome.Name = "editCognome";
+            this.editCognome.ReadOnly = false;
+            this.editCognome.Required = false;
+            this.editCognome.Size = new System.Drawing.Size(800, 30);
+            this.editCognome.TabIndex = 3;
+            this.editCognome.Text = "EditControl";
+            this.editCognome.Value = "";
+            // 
+            // editConfermaPassword
+            // 
+            this.editConfermaPassword.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editConfermaPassword.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editConfermaPassword.BackColor = System.Drawing.Color.Transparent;
+            this.editConfermaPassword.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editConfermaPassword.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editConfermaPassword.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editConfermaPassword.Changed = false;
+            this.editConfermaPassword.Editing = true;
+            this.editConfermaPassword.Label = "Conferma password";
+            this.editConfermaPassword.LabelWidth = 175;
+            this.editConfermaPassword.Location = new System.Drawing.Point(20, 279);
+            this.editConfermaPassword.Name = "editConfermaPassword";
+            this.editConfermaPassword.Password = true;
+            this.editConfermaPassword.ReadOnly = false;
+            this.editConfermaPassword.Required = false;
+            this.editConfermaPassword.Size = new System.Drawing.Size(800, 30);
+            this.editConfermaPassword.TabIndex = 4;
+            this.editConfermaPassword.Text = "EditControl";
+            this.editConfermaPassword.Value = "";
+            // 
+            // editPassword
+            // 
+            this.editPassword.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editPassword.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editPassword.BackColor = System.Drawing.Color.Transparent;
+            this.editPassword.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editPassword.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editPassword.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editPassword.Changed = false;
+            this.editPassword.Editing = false;
+            this.editPassword.Label = "Password";
+            this.editPassword.LabelWidth = 175;
+            this.editPassword.Location = new System.Drawing.Point(20, 228);
+            this.editPassword.Name = "templateEditPassword2";
+            this.editPassword.Password = true;
+            this.editPassword.ReadOnly = false;
+            this.editPassword.Required = false;
+            this.editPassword.Size = new System.Drawing.Size(800, 30);
+            this.editPassword.TabIndex = 5;
+            this.editPassword.Text = "EditControl";
+            this.editPassword.Value = "";
+            // 
+            // editNome
+            // 
+            this.editNome.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editNome.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editNome.BackColor = System.Drawing.Color.Transparent;
+            this.editNome.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editNome.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editNome.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editNome.Changed = false;
+            this.editNome.Editing = false;
+            this.editNome.Label = "Nome";
+            this.editNome.LabelWidth = 175;
+            this.editNome.Location = new System.Drawing.Point(20, 126);
+            this.editNome.Multiline = false;
+            this.editNome.Name = "editNome";
+            this.editNome.ReadOnly = false;
+            this.editNome.Required = false;
+            this.editNome.Size = new System.Drawing.Size(800, 30);
+            this.editNome.TabIndex = 6;
+            this.editNome.Text = "EditControl";
+            this.editNome.Value = "";
+            // 
+            // editNomeUtente
+            // 
+            this.editNomeUtente.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editNomeUtente.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editNomeUtente.BackColor = System.Drawing.Color.Transparent;
+            this.editNomeUtente.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editNomeUtente.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editNomeUtente.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editNomeUtente.Changed = false;
+            this.editNomeUtente.Editing = false;
+            this.editNomeUtente.Label = "Nome utente";
+            this.editNomeUtente.LabelWidth = 175;
+            this.editNomeUtente.Location = new System.Drawing.Point(20, 177);
+            this.editNomeUtente.Multiline = false;
+            this.editNomeUtente.Name = "editNomeUtente";
+            this.editNomeUtente.ReadOnly = false;
+            this.editNomeUtente.Required = false;
+            this.editNomeUtente.Size = new System.Drawing.Size(800, 30);
+            this.editNomeUtente.TabIndex = 7;
+            this.editNomeUtente.Text = "EditControl";
+            this.editNomeUtente.Value = "";
+            // 
+            // editTelefono
+            // 
+            this.editTelefono.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editTelefono.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editTelefono.BackColor = System.Drawing.Color.Transparent;
+            this.editTelefono.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editTelefono.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editTelefono.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editTelefono.Changed = false;
+            this.editTelefono.Editing = false;
+            this.editTelefono.Label = "Telefono";
+            this.editTelefono.LabelWidth = 175;
+            this.editTelefono.Location = new System.Drawing.Point(20, 381);
+            this.editTelefono.Multiline = false;
+            this.editTelefono.Name = "editTelefono";
+            this.editTelefono.ReadOnly = false;
+            this.editTelefono.Required = false;
+            this.editTelefono.Size = new System.Drawing.Size(800, 30);
+            this.editTelefono.TabIndex = 8;
+            this.editTelefono.Text = "EditControl";
+            this.editTelefono.Value = "";
+            // 
+            // editAmministratore
+            // 
+            this.editAmministratore.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editAmministratore.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editAmministratore.BackColor = System.Drawing.Color.Transparent;
+            this.editAmministratore.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editAmministratore.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editAmministratore.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editAmministratore.Changed = false;
+            this.editAmministratore.Editing = false;
+            this.editAmministratore.Label = "Amministratore";
+            this.editAmministratore.LabelWidth = 175;
+            this.editAmministratore.Location = new System.Drawing.Point(20, 483);
+            this.editAmministratore.Name = "editAmministratore";
+            this.editAmministratore.ReadOnly = false;
+            this.editAmministratore.Required = false;
+            this.editAmministratore.Size = new System.Drawing.Size(424, 30);
+            this.editAmministratore.TabIndex = 9;
+            this.editAmministratore.Text = "TemplateCheckBox";
+            this.editAmministratore.TextFalse = "";
+            this.editAmministratore.TextTrue = "";
+            this.editAmministratore.Value = null;
+            // 
+            // editUtente
+            // 
+            this.editUtente.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editUtente.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editUtente.BackColor = System.Drawing.Color.Transparent;
+            this.editUtente.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editUtente.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editUtente.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editUtente.Changed = false;
+            this.editUtente.Editing = false;
+            this.editUtente.Label = "Utente";
+            this.editUtente.LabelWidth = 175;
+            this.editUtente.Location = new System.Drawing.Point(20, 534);
+            this.editUtente.Name = "editUtente";
+            this.editUtente.ReadOnly = false;
+            this.editUtente.Required = false;
+            this.editUtente.Size = new System.Drawing.Size(424, 30);
+            this.editUtente.TabIndex = 10;
+            this.editUtente.Text = "TemplateCheckBox";
+            this.editUtente.TextFalse = "";
+            this.editUtente.TextTrue = "";
+            this.editUtente.Value = null;
+            // 
+            // editSupervisor
+            // 
+            this.editSupervisor.Anchor = ((Gizmox.WebGUI.Forms.AnchorStyles)(((Gizmox.WebGUI.Forms.AnchorStyles.Top | Gizmox.WebGUI.Forms.AnchorStyles.Left) 
+            | Gizmox.WebGUI.Forms.AnchorStyles.Right)));
+            this.editSupervisor.AutoValidate = Gizmox.WebGUI.Forms.AutoValidate.EnablePreventFocusChange;
+            this.editSupervisor.BackColor = System.Drawing.Color.Transparent;
+            this.editSupervisor.BorderColor = new Gizmox.WebGUI.Forms.BorderColor(System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224))))));
+            this.editSupervisor.BorderStyle = Gizmox.WebGUI.Forms.BorderStyle.FixedSingle;
+            this.editSupervisor.BorderWidth = new Gizmox.WebGUI.Forms.BorderWidth(0, 0, 0, 1);
+            this.editSupervisor.Changed = false;
+            this.editSupervisor.Editing = false;
+            this.editSupervisor.Label = "Supervisor";
+            this.editSupervisor.LabelWidth = 175;
+            this.editSupervisor.Location = new System.Drawing.Point(20, 432);
+            this.editSupervisor.Name = "editSupervisor";
+            this.editSupervisor.ReadOnly = false;
+            this.editSupervisor.Required = false;
+            this.editSupervisor.Size = new System.Drawing.Size(424, 30);
+            this.editSupervisor.TabIndex = 11;
+            this.editSupervisor.Text = "TemplateCheckBox";
+            this.editSupervisor.TextFalse = "";
+            this.editSupervisor.TextTrue = "";
+            this.editSupervisor.Value = null;
             this.Controls.SetChildIndex(this.container, 0);
             this.container.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.infoSubtitleImage)).EndInit();
@@ -60,6 +316,19 @@ namespace Web.GUI.Operatore
 		}
 
 		#endregion
+
+        private Library.Template.Controls.TemplateEditText editNomeUtente;
+        private Library.Template.Controls.TemplateEditText editNome;
+        private Library.Template.Controls.TemplateEditPassword templateEditPassword2;
+        private Library.Template.Controls.TemplateEditPassword editConfermaPassword;
+        private Library.Template.Controls.TemplateEditText editCognome;
+        private Library.Template.Controls.TemplateEditEmail templateEditEmail1;
+        private Library.Template.Controls.TemplateEditText editTelefono;
+        private Library.Template.Controls.TemplateEditPassword editPassword;
+        private Library.Template.Controls.TemplateEditEmail editEmail;
+        private Library.Template.Controls.TemplateEditCheckBox editSupervisor;
+        private Library.Template.Controls.TemplateEditCheckBox editUtente;
+        private Library.Template.Controls.TemplateEditCheckBox editAmministratore;
 
 
     }
